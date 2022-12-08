79ec1aba1d30d6183947c25ff2c9c504df65baa7
Shanas <nassibshaban345@gmail.com>
2022-12-08 15:15:47 +0300

    Done edit admin page

--- app/(main)/admins/edit/[id]/page.tsx
+++ app/(main)/admins/edit/[id]/page.tsx
@@ -1,7 +1,70 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { BreadCrumb, FormNav, TextInput } from "@components/index";
+import { crumbs } from "@lib/dummy";
+
 export default function Page() {
+  const { push } = useRouter();
+
   return (
-    <>
-      <h1>Edit admin</h1>
-    </>
+    <section className="w-full ml-6">
+      <header>
+        <BreadCrumb crumbs={crumbs} />
+      </header>
+
+      <main>
+        <h2 className="text-2xl font-semibold">Edit Admin</h2>
+
+        <form className="flex flex-col gap-y-6 my-4 w-[95%] md:w-3/5">
+          <TextInput inputType="number" placeholder="Employee ID" value={123} />
+          <TextInput inputType="text" placeholder="First name" value="Tahir" />
+          <TextInput inputType="text" placeholder="Last name" value="Ramzan" />
+          <TextInput
+            inputType="email"
+            placeholder="Email"
+            value="tahir@nappr.io"
+          />
+
+          {/* Dropdown  */}
+
+          <div className="flex items-center">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                className="form-select bg-lightGray border-none py-3 rounded-md m-1"
+              >
+                +1
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-2 shadow bg-lightGray rounded-box w-52"
+              >
+                <li>
+                  <a>+255</a>
+                </li>
+                <li>
+                  <a>+254</a>
+                </li>
+              </ul>
+            </div>
+            <TextInput
+              inputType="tel"
+              placeholder="Mobile no"
+              value={+18398484848}
+            />
+          </div>
+
+          {/* Form navigation  */}
+
+          <FormNav
+            rightBtnText="Next"
+            // redirect to verify admin page
+            rightBtnAction={() => push("/admins/add/verify")}
+          />
+        </form>
+      </main>
+    </section>
   );
 }
--- components/text-input.tsx
+++ components/text-input.tsx
@@ -1,12 +1,14 @@
 type Props = {
   placeholder: string;
   inputType: string;
+  value?: any;
 };
 
-const TextInput = ({ placeholder, inputType }: Props) => {
+const TextInput = ({ placeholder, inputType, value }: Props) => {
   return (
     <div className="w-full">
       <input
+        value={value}
         type={inputType}
         className="outline-none border-none focus:outline-none focus:border-none w-full bg-lightGray p-3 rounded-md"
         placeholder={placeholder}
